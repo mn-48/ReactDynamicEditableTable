3f2205c4ba1f231b5bf07d6acbdf9351e0e61ad5
MdNazmul9 <nazmul.cse.pust@gmail.com>
2023-07-04 18:43:55 +0600

    init code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TableDemo from "./TableDemo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			{/* Header with inline css */}
+			<h1
+				style={{
+					display: 'flex', justifyContent: 'center', padding: '15px',
+					border: '13px solid #b4f0b4', color: 'rgb(11, 167, 11)'
+				}}>
+				 Material UI Dynamic Editable Table
+			</h1>
+			{/* Table component below header */}
+			<TableDemo />
+		</div>
+	)
 }
 
 export default App;
